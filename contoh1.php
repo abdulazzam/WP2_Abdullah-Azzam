58ce0bdc58f4fe7c5e50c9c877cf51af1b38700e
abdulazzam <101967083+abdulazzam@users.noreply.github.com>
2022-03-20 22:55:49 +0700

    Ini file tugas pertemuan 2

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Abdullah Azzam
+            saya tinggal di depok
+            saya tidak gay
+            saya fetish bultang";
+    }
+}
